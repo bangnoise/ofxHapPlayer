bangnoise/ofxHapPlayer

commit 66680da3423b48f74714e20d57c7b438d6ee2d50

Functioning audio on Linux
Support the older libavcodec decode API, query frame PTS properly

libs/ofxHap/src/AudioThread.cpp
@@ -82,8 +82,8 @@ void ofxHap::AudioThread::threadMain(AudioParameters params, int outRate, std::s
         int sampleRate = params.parameters->sample_rate;
         int channels = params.parameters->channels;
 #else
-        int sampleRate = params.sample_rate;
-        int channels = params.channels;
+        int sampleRate = params.context->sample_rate;
+        int channels = params.context->channels;
 #endif
         AudioResampler resampler(params, outRate);
         int64_t maxDelayScaled = av_rescale_q(buffer->getSamplesPerChannel(), AV_TIME_BASE_Q, {1, sampleRate});
@@ -109,7 +109,8 @@ void ofxHap::AudioThread::threadMain(AudioParameters params, int outRate, std::s
                         result = decoder.receive(frame);
                         if (result >= 0)
                         {
-                            auto r = cache.insert(std::map<int64_t, AVFrame *>::value_type(frame->pts, frame));
+                            int64_t ts = av_frame_get_best_effort_timestamp(frame);
+                            auto r = cache.insert(std::map<int64_t, AVFrame *>::value_type(ts, frame));
                             if (r.second == false)
                             {
                                 // TODO: could we be refusing to add one with more samples than another?
@@ -181,11 +182,12 @@ void ofxHap::AudioThread::threadMain(AudioParameters params, int outRate, std::s
                         else
                         {
                             AVFrame *frame = nullptr;
-
+                            int64_t pts;
                             for (const auto pair : cache) {
-                                if (pair.first <= start && pair.second->pts + pair.second->nb_samples > start)
+                                if (pair.first <= start && pair.first + pair.second->nb_samples > start)
                                 {
                                     frame = pair.second;
+                                    pts = pair.first;
                                     break;
                                 }
                             }
@@ -195,15 +197,15 @@ void ofxHap::AudioThread::threadMain(AudioParameters params, int outRate, std::s
                                 if (forwards)
                                 {
                                     // Fill forwards
-                                    lengthIn = std::min(lengthIn, static_cast<int>(frame->nb_samples - (start - frame->pts)));
+                                    lengthIn = std::min(lengthIn, static_cast<int>(frame->nb_samples - (start - pts)));
 
                                     // AAAAND so we'll need to know about discontinuities and flush the resampler
-                                    result = resampler.resample(frame, start - frame->pts, lengthIn, dst[i], count[i], lengthOut, lengthIn);
+                                    result = resampler.resample(frame, start - pts, lengthIn, dst[i], count[i], lengthOut, lengthIn);
                                 }
                                 else
                                 {
                                     // Fill backwards
-                                    lengthIn = std::min(lengthIn, static_cast<int>(start - frame->pts) + 1);
+                                    lengthIn = std::min(lengthIn, static_cast<int>(start - pts) + 1);
                                     if (reversed == nullptr)
                                     {
                                         reversed = av_frame_alloc();
@@ -212,7 +214,9 @@ void ofxHap::AudioThread::threadMain(AudioParameters params, int outRate, std::s
                                             result = AVERROR(ENOMEM);
                                         }
                                     }
-                                    if (result >= 0 && reversed->pts != frame->pts)
+
+                                    int rpts = av_frame_get_best_effort_timestamp(reversed);
+                                    if (result >= 0 && rpts != pts)
                                     {
                                         result = reverse(reversed, frame);
                                     }
@@ -223,7 +227,7 @@ void ofxHap::AudioThread::threadMain(AudioParameters params, int outRate, std::s
                                     // check that
                                     if (result >= 0)
                                     {
-                                        int offset = reversed->pts + reversed->nb_samples - 1 - start;
+                                        int offset = rpts + reversed->nb_samples - 1 - start;
                                         result = resampler.resample(reversed, offset, lengthIn, dst[i], count[i], lengthOut, lengthIn);
                                     }
                                 }

libs/ofxHap/include/ofxHap/AudioDecoder.h
@@ -28,7 +28,6 @@
 
 #ifndef AudioDecoder_h
 #define AudioDecoder_h
-#include <vector>
 #include "AudioParameters.h"
 
 typedef struct AVCodecContext AVCodecContext;
@@ -45,6 +44,11 @@ namespace ofxHap {
         void flush();
     private:
         AVCodecContext *_codec_ctx;
+#if !OFX_HAP_HAS_CODECPAR
+        int decode(AVPacket *packet);
+        AVPacket *_packet;
+        AVFrame *_frame;
+#endif
     };
 }
 

libs/ofxHap/include/ofxHap/AudioParameters.h
@@ -33,6 +33,8 @@
 
 #if OFX_HAP_HAS_CODECPAR
 typedef struct AVCodecParameters AVCodecParameters;
+#else
+typedef struct AVCodecContext AVCodecContext;
 #endif
 
 namespace ofxHap {
@@ -42,17 +44,14 @@ class AudioParameters
 public:
 #if OFX_HAP_HAS_CODECPAR
 	AudioParameters(AVCodecParameters *p);
-	AudioParameters(const AudioParameters& o);
-	AudioParameters& operator=(const AudioParameters& o);
-	~AudioParameters();
 	AVCodecParameters* parameters;
 #else
-	int channels;
-	int sample_rate;
-	int codec_id;
-	int format;
-	uint64_t channel_layout;
+	AudioParameters(AVCodecContext *c);
+	AVCodecContext *context;
 #endif
+	~AudioParameters();
+	AudioParameters(const AudioParameters& o);
+	AudioParameters& operator=(const AudioParameters& o);
 };
 
 }

libs/ofxHap/src/AudioDecoder.cpp
@@ -34,6 +34,14 @@ extern "C" {
 
 ofxHap::AudioDecoder::AudioDecoder(const AudioParameters& params, int& result)
 : _codec_ctx(nullptr)
+#if !OFX_HAP_HAS_CODECPAR
+#if OFX_HAP_HAS_PACKET_ALLOC
+, _packet(av_packet_alloc()),
+#else
+, _packet(static_cast<AVPacket *>(av_malloc(sizeof(AVPacket)))),
+#endif
+  _frame(av_frame_alloc())
+#endif
 {
     result = 0;
     _codec_ctx = avcodec_alloc_context3(nullptr);
@@ -44,7 +52,12 @@ ofxHap::AudioDecoder::AudioDecoder(const AudioParameters& params, int& result)
 #if OFX_HAP_HAS_CODECPAR
     AVCodec *decoder = avcodec_find_decoder(params.parameters->codec_id);
 #else
-    AVCodec *decoder = avcodec_find_decoder(static_cast<AVCodecID>(params.codec_id));
+#if !OFX_HAP_HAS_PACKET_ALLOC
+    av_init_packet(_packet);
+#endif
+    _packet->data = nullptr;
+    _packet->size = 0;
+    AVCodec *decoder = avcodec_find_decoder(static_cast<AVCodecID>(params.context->codec_id));
 #endif
     if (!decoder && result >= 0)
     {
@@ -55,7 +68,7 @@ ofxHap::AudioDecoder::AudioDecoder(const AudioParameters& params, int& result)
 #if OFX_HAP_HAS_CODECPAR
         result = avcodec_parameters_to_context(_codec_ctx, params.parameters);
 #else
-        // TODO: do we need to avcodec_copy_context() from the demuxer's context?
+        result = avcodec_copy_context(_codec_ctx, params.context);
 #endif
         if (result >= 0)
         {
@@ -76,14 +89,76 @@ ofxHap::AudioDecoder::~AudioDecoder()
     {
         avcodec_free_context(&_codec_ctx);
     }
+#if !OFX_HAP_HAS_CODECPAR
+#if OFX_HAP_HAS_PACKET_ALLOC
+    av_packet_free(&_packet);
+#else
+    av_packet_unref(_packet);
+    av_freep(&_packet);
+#endif
+    av_frame_free(&_frame);
+#endif
 }
+
+#if !OFX_HAP_HAS_CODECPAR
+int ofxHap::AudioDecoder::decode(AVPacket *packet)
+{
+    if (!packet)
+    {
+        packet = _packet;
+    }
+
+    int got;
+    int result = avcodec_decode_audio4(_codec_ctx, _frame, &got, packet);
+
+    if (result == AVERROR(EAGAIN))
+    {
+        result = packet->size;
+    }
+    if (result < 0)
+    {
+        return result;
+    }
+
+    if (result >= packet->size)
+    {
+        av_packet_unref(_packet);
+    }
+    else
+    {
+        if (packet != _packet)
+        {
+            av_packet_unref(_packet);
+            int reffed = av_packet_ref(_packet, packet);
+            if (reffed < 0)
+            {
+                return reffed;
+            }
+        }
+
+        _packet->data += result;
+        _packet->size -= result;
+        _packet->pts = AV_NOPTS_VALUE;
+        _packet->dts = AV_NOPTS_VALUE;
+    }
+
+    return 0;
+}
+#endif
 
 int ofxHap::AudioDecoder::send(AVPacket *packet)
 {
 #if OFX_HAP_HAS_CODECPAR
     return avcodec_send_packet(_codec_ctx, packet);
 #else
-    // TODO: 
+    if (_packet->size > 0 || _frame->data[0])
+    {
+        return AVERROR(EAGAIN);
+    }
+    else
+    {
+        return decode(packet);
+    }
 #endif
 }
 
@@ -92,10 +167,37 @@ int ofxHap::AudioDecoder::receive(AVFrame *frame)
 #if OFX_HAP_HAS_CODECPAR
     int result = avcodec_receive_frame(_codec_ctx, frame);
 #else
-    // TODO: 
-    int result = AVERROR_PATCHWELCOME;
+
+    av_frame_unref(frame);
+
+    if (!_frame->data[0])
+    {
+        if (_packet->size == 0)
+        {
+            return AVERROR(EAGAIN);
+        }
+
+        do
+        {
+            int result = decode(_packet);
+            if (result < 0)
+            {
+                av_packet_unref(_packet);
+                return result;
+            }
+        } while (!_frame->data[0] && _packet->size > 0);
+    }
+
+    if (!_frame->data[0])
+    {
+        // TODO: what about EOF? should we check for previous EOF return
+        // then return that after
+        return AVERROR(EAGAIN);
+    }
+
+    av_frame_move_ref(frame, _frame);
+    return 0;
 #endif
-    return result;
 }
 
 void ofxHap::AudioDecoder::flush()

libs/ofxHap/src/AudioParameters.cpp
@@ -25,13 +25,13 @@
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
- #include "AudioParameters.h"
-
-#if OFX_HAP_HAS_CODECPAR
+#include "AudioParameters.h"
 
 extern "C" {
 #include <libavformat/avformat.h>
 }
+
+#if OFX_HAP_HAS_CODECPAR
 
 ofxHap::AudioParameters::AudioParameters(AVCodecParameters* p)
 : parameters(avcodec_parameters_alloc())
@@ -56,6 +56,33 @@ ofxHap::AudioParameters& ofxHap::AudioParameters::operator=(const AudioParameter
 	avcodec_parameters_copy(p, parameters);
 	avcodec_parameters_free(&parameters);
 	parameters = p;
+}
+
+#else
+
+ofxHap::AudioParameters::AudioParameters(AVCodecContext* c)
+: context(avcodec_alloc_context3(avcodec_find_decoder(c->codec_id)))
+{
+	avcodec_copy_context(context, c);
+}
+
+ofxHap::AudioParameters::~AudioParameters()
+{
+	avcodec_free_context(&context);
+}
+
+ofxHap::AudioParameters::AudioParameters(const AudioParameters& o)
+: context(avcodec_alloc_context3(avcodec_find_decoder(o.context->codec_id)))
+{
+	avcodec_copy_context(context, o.context);
+}
+
+ofxHap::AudioParameters& ofxHap::AudioParameters::operator=(const AudioParameters& o)
+{
+	AVCodecContext *c = avcodec_alloc_context3(avcodec_find_decoder(o.context->codec_id));
+	avcodec_copy_context(c, o.context);
+	avcodec_free_context(&context);
+	context = c;
 }
 
 #endif

libs/ofxHap/src/AudioResampler.cpp
@@ -38,7 +38,7 @@ ofxHap::AudioResampler::AudioResampler(const AudioParameters& params, int outRat
 #if OFX_HAP_HAS_CODECPAR
 _layout(params.parameters->channel_layout), _sampleRateIn(params.parameters->sample_rate), _sampleRateOut(outRate), _format(params.parameters->format)
 #else
-_layout(params.channel_layout), _sampleRateIn(params.sample_rate), _sampleRateOut(outRate), _format(params.format)
+_layout(params.context->channel_layout), _sampleRateIn(params.context->sample_rate), _sampleRateOut(outRate), _format(params.context->sample_fmt)
 #endif
 {
 

src/ofxHapPlayer.cpp
@@ -168,12 +168,7 @@ void ofxHapPlayer::foundStream(AVStream *stream)
     }
     else if (codec->codec_type == AVMEDIA_TYPE_AUDIO)
     {
-        ofxHap::AudioParameters p;
-        p.channels = codec->channels;
-        p.sample_rate = codec->sample_rate;
-        p.codec_id = codec->codec_id;
-        p.format = codec->sample_fmt;
-        p.channel_layout = codec->channel_layout;
+        ofxHap::AudioParameters p(codec);
 
         // TODO: we almost don't need to store this, only to identify packet index
         _audioStream = stream;

commit ac6ea7317e6d691cb931ef204be5596cc7441dca

Fix reversed audio playback
Broken since 66680da3423b48f74714e20d57c7b438d6ee2d50

libs/ofxHap/src/AudioThread.cpp
@@ -219,6 +219,7 @@ void ofxHap::AudioThread::threadMain(AudioParameters params, int outRate, std::s
                                     if (result >= 0 && rpts != pts)
                                     {
                                         result = reverse(reversed, frame);
+                                        rpts = av_frame_get_best_effort_timestamp(reversed);
                                     }
 
                                     // maybe resampler knows pts, detects non-contiguous blocks, resets itself