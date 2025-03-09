bangnoise/ofxHapPlayer

commit 8dadcce9afd78582b815b22f7e47ae973c65b333

Source support for newer ffmpeg libraries

libs/ofxHap/src/AudioResampler.cpp
@@ -37,11 +37,18 @@ extern "C" {
 ofxHap::AudioResampler::AudioResampler(const AudioParameters& params, int outRate)
 : _volume(1.0), _rate(1.0), _resampler(nullptr), _reconfigure(true),
 #if OFX_HAP_HAS_CODECPAR
-_layout(params.parameters->channel_layout), _sampleRateIn(params.parameters->sample_rate), _sampleRateOut(outRate), _format(params.parameters->format)
+#if OFX_HAP_HAS_CHANNEL_LAYOUT
+_layout(&params.parameters->ch_layout),
 #else
-_layout(params.context->channel_layout), _sampleRateIn(params.context->sample_rate), _sampleRateOut(outRate), _format(params.context->sample_fmt)
+_layout(&params.parameters->channel_layout),
 #endif
+_sampleRateIn(params.parameters->sample_rate), _format(params.parameters->format),
+#else
+_layout(params.context->channel_layout), _sampleRateIn(params.context->sample_rate), _format(params.context->sample_fmt),
+#endif
+_sampleRateOut(outRate)
 {
+#if !OFX_HAP_HAS_CHANNEL_LAYOUT
     if (_layout == 0)
     {
 #if OFX_HAP_HAS_CODECPAR
@@ -50,6 +57,7 @@ _layout(params.context->channel_layout), _sampleRateIn(params.context->sample_ra
         _layout = av_get_default_channel_layout(params.context->channels);
 #endif
     }
+#endif
 }
 
 ofxHap::AudioResampler::~AudioResampler()
@@ -100,6 +108,19 @@ int ofxHap::AudioResampler::resample(const AVFrame *frame, int offset, int srcLe
         {
             swr_free(&_resampler);
         }
+#if OFX_HAP_HAS_CHANNEL_LAYOUT
+        int res = swr_alloc_set_opts2(&_resampler,
+                                      _layout,
+                                      AV_SAMPLE_FMT_FLT,
+                                      static_cast<int>(_sampleRateOut / _rate),
+                                      _layout,
+                                      static_cast<AVSampleFormat>(_format),
+                                      _sampleRateIn,
+                                      0,
+                                      nullptr);
+        
+        int channels = _layout->nb_channels;
+#else
         _resampler = swr_alloc_set_opts(nullptr,
                                         _layout,
                                         AV_SAMPLE_FMT_FLT,
@@ -111,6 +132,7 @@ int ofxHap::AudioResampler::resample(const AVFrame *frame, int offset, int srcLe
                                         nullptr);
 
         int channels = av_get_channel_layout_nb_channels(_layout);
+#endif
         std::vector<double> matrix(channels * channels);
         for (int i = 0; i < channels; i++) {
             for (int j = 0; j < channels; j++) {
@@ -133,16 +155,21 @@ int ofxHap::AudioResampler::resample(const AVFrame *frame, int offset, int srcLe
         _reconfigure = false;
     }
 
-    std::vector<const uint8_t *> src(frame->channels);
+#if OFX_HAP_HAS_CHANNEL_LAYOUT
+    int nb_channels = frame->ch_layout.nb_channels;
+#else
+    int nb_channels = frame->channels;
+#endif
+    std::vector<const uint8_t *> src(nb_channels);
     if (av_sample_fmt_is_planar(static_cast<AVSampleFormat>(frame->format)))
     {
-        for (int i = 0; i < frame->channels; i++) {
+        for (int i = 0; i < nb_channels; i++) {
             src[i] = frame->extended_data[i] + (offset * av_get_bytes_per_sample(static_cast<AVSampleFormat>(frame->format)));
         }
     }
     else
     {
-        src[0] = frame->data[0] + (offset * av_get_bytes_per_sample(static_cast<AVSampleFormat>(frame->format)) * frame->channels);
+        src[0] = frame->data[0] + (offset * av_get_bytes_per_sample(static_cast<AVSampleFormat>(frame->format)) * nb_channels);
     }
 
     int result = swr_convert(_resampler,

libs/ofxHap/include/ofxHap/AudioResampler.h
@@ -33,6 +33,9 @@
 
 typedef struct SwrContext SwrContext;
 typedef struct AVFrame AVFrame;
+#if OFX_HAP_HAS_CHANNEL_LAYOUT
+typedef struct AVChannelLayout AVChannelLayout;
+#endif
 
 namespace ofxHap {
     class AudioResampler {
@@ -50,7 +53,11 @@ namespace ofxHap {
         float       _rate;
         SwrContext *_resampler;
         bool        _reconfigure;
+#if OFX_HAP_HAS_CHANNEL_LAYOUT
+        AVChannelLayout *_layout;
+#else
         uint64_t    _layout;
+#endif
         int         _sampleRateIn;
         int         _sampleRateOut;
         int         _format;

libs/ofxHap/include/ofxHap/Common.h
@@ -44,4 +44,10 @@ extern "C" {
 #define OFX_HAP_HAS_PACKET_ALLOC 0
 #endif
 
+#if (LIBAVCODEC_VERSION_MAJOR > 59 || (LIBAVCODEC_VERSION_MAJOR == 59 && LIBAVCODEC_VERSION_MINOR >= 24))
+#define OFX_HAP_HAS_CHANNEL_LAYOUT 1
+#else
+#define OFX_HAP_HAS_CHANNEL_LAYOUT 0
+#endif
+
 #endif

libs/ofxHap/src/AudioDecoder.cpp
@@ -28,6 +28,7 @@
 
 #include <ofxHap/AudioDecoder.h>
 extern "C" {
+#include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libavutil/time.h>
 }

libs/ofxHap/src/AudioThread.cpp
@@ -83,7 +83,11 @@ void ofxHap::AudioThread::threadMain(AudioParameters params, int outRate, std::s
     {
 #if OFX_HAP_HAS_CODECPAR
         int sampleRate = params.parameters->sample_rate;
+#if OFX_HAP_HAS_CHANNEL_LAYOUT
+        int channels = params.parameters->ch_layout.nb_channels;
+#else
         int channels = params.parameters->channels;
+#endif
 #else
         int sampleRate = params.context->sample_rate;
         int channels = params.context->channels;
@@ -440,8 +444,12 @@ int ofxHap::AudioThread::reverse(AVFrame *dst, const AVFrame *src)
         av_frame_unref(dst);
         if (result >= 0)
         {
+#if OFX_HAP_HAS_CHANNEL_LAYOUT
+            dst->ch_layout = src->ch_layout;
+#else
             dst->channel_layout = src->channel_layout;
             dst->channels = src->channels;
+#endif
             dst->format = src->format;
             dst->nb_samples = src->nb_samples;
             result = av_frame_get_buffer(dst, 1);
@@ -461,9 +469,14 @@ int ofxHap::AudioThread::reverse(AVFrame *dst, const AVFrame *src)
         result = av_frame_copy_props(dst, src);
         if (av_sample_fmt_is_planar(static_cast<AVSampleFormat>(src->format)))
         {
+#if OFX_HAP_HAS_CHANNEL_LAYOUT
+            int nb_channels = dst->ch_layout.nb_channels;
+#else
+            int nb_channels = dst->channels;
+#endif
             // TODO: these needn't use memcpy if we deal with different formats
             int bps = av_get_bytes_per_sample(static_cast<AVSampleFormat>(src->format));
-            for (int i = 0; i < dst->channels; i++) {
+            for (int i = 0; i < nb_channels; i++) {
                 for (int j = 0, k = dst->nb_samples - 1; j < dst->nb_samples; j++, k--) {
                     memcpy(&dst->extended_data[i][j * bps], &src->extended_data[i][k * bps], bps);
                 }
@@ -471,7 +484,12 @@ int ofxHap::AudioThread::reverse(AVFrame *dst, const AVFrame *src)
         }
         else
         {
-            int bpcs = av_get_bytes_per_sample(static_cast<AVSampleFormat>(src->format)) * src->channels;
+#if OFX_HAP_HAS_CHANNEL_LAYOUT
+            int nb_channels = src->ch_layout.nb_channels;
+#else
+            int nb_channels = src->channels;
+#endif
+            int bpcs = av_get_bytes_per_sample(static_cast<AVSampleFormat>(src->format)) * nb_channels;
             for (int i = 0, j = dst->nb_samples - 1; i < dst->nb_samples; i++, j--) {
                 memcpy(&dst->data[0][i * bpcs], &src->data[0][j * bpcs], bpcs);
             }

libs/ofxHap/src/Demuxer.cpp
@@ -121,8 +121,7 @@ void ofxHap::Demuxer::threadMain(const std::string movie, PacketReceiver& receiv
             int64_t lastReadVideo = AV_NOPTS_VALUE;
             int64_t lastReadAudio = AV_NOPTS_VALUE;
 
-            AVPacket packet;
-            av_init_packet(&packet);
+            AVPacket *packet = av_packet_alloc();
             
             while (!finish)
             {
@@ -145,20 +144,20 @@ void ofxHap::Demuxer::threadMain(const std::string movie, PacketReceiver& receiv
                             if ((lastReadVideo == AV_NOPTS_VALUE || lastReadVideo < action.pts) ||
                                 (audioStreamIndex >= 0 && (lastReadAudio == AV_NOPTS_VALUE || lastReadAudio < action.pts)))
                             {
-                                packet.data = NULL;
-                                packet.size = 0;
-                                result = av_read_frame(fmt_ctx, &packet);
+                                packet->data = NULL;
+                                packet->size = 0;
+                                result = av_read_frame(fmt_ctx, packet);
                                 if (result >= 0)
                                 {
-                                    receiver.readPacket(&packet);
-                                    if (packet.stream_index == videoStreamIndex)
+                                    receiver.readPacket(packet);
+                                    if (packet->stream_index == videoStreamIndex)
                                     {
-                                        lastReadVideo = av_rescale_q(packet.pts + packet.duration - 1,
+                                        lastReadVideo = av_rescale_q(packet->pts + packet->duration - 1,
                                                                      fmt_ctx->streams[videoStreamIndex]->time_base, { 1, AV_TIME_BASE });
                                     }
-                                    else if (packet.stream_index == audioStreamIndex)
+                                    else if (packet->stream_index == audioStreamIndex)
                                     {
-                                        lastReadAudio = av_rescale_q(packet.pts + packet.duration - 1,
+                                        lastReadAudio = av_rescale_q(packet->pts + packet->duration - 1,
                                                                      fmt_ctx->streams[audioStreamIndex]->time_base, { 1, AV_TIME_BASE });
                                     }
                                 }
@@ -166,7 +165,7 @@ void ofxHap::Demuxer::threadMain(const std::string movie, PacketReceiver& receiv
                                 {
                                     receiver.endMovie();
                                 }
-                                av_packet_unref(&packet);
+                                av_packet_unref(packet);
                             }
                             break;
                         default:
@@ -218,6 +217,7 @@ void ofxHap::Demuxer::threadMain(const std::string movie, PacketReceiver& receiv
                     }
                 }
             }
+            av_packet_free(&packet);
         }
 
         if (fmt_ctx)

src/ofxHapPlayer.cpp
@@ -215,7 +215,11 @@ void ofxHapPlayer::foundStream(AVStream *stream)
     {
         // We will output silence until we have samples to play
 #if OFX_HAP_HAS_CODECPAR
+#if OFX_HAP_HAS_CHANNEL_LAYOUT
+        int channels = params->ch_layout.nb_channels;
+#else
         int channels = params->channels;
+#endif
         int sampleRate = params->sample_rate;
         ofxHap::AudioParameters parameters(params, kofxHapPlayerBufferUSec, stream->start_time, stream->duration);
 #else
@@ -386,24 +390,23 @@ void ofxHapPlayer::update(ofEventArgs & args)
         bool inBuffer = (_decodedFrame.isValid() && _decodedFrame.pts <= vidPosition && _decodedFrame.pts + _decodedFrame.duration > vidPosition) ? true : false;
         if (!inBuffer)
         {
-            AVPacket packet;
-            av_init_packet(&packet);
-            packet.data = NULL;
-            packet.size = 0;
+            AVPacket *packet = av_packet_alloc();
+            packet->data = NULL;
+            packet->size = 0;
             // Fetch a stored packet, blocking until our timeout only if necessary
-            bool found = _videoPackets.fetch(vidPosition, &packet);
+            bool found = _videoPackets.fetch(vidPosition, packet);
             if (!found && _demuxer->isActive())
             {
-                found = _videoPackets.fetch(vidPosition, &packet, _timeout);
+                found = _videoPackets.fetch(vidPosition, packet, _timeout);
             }
             if (found)
             {
                 unsigned int textureCount;
-                unsigned int hapResult = HapGetFrameTextureCount(packet.data, packet.size, &textureCount);
+                unsigned int hapResult = HapGetFrameTextureCount(packet->data, packet->size, &textureCount);
                 if (hapResult == HapResult_No_Error && textureCount == 1) // TODO: Hap Q+A
                 {
                     unsigned int textureFormat;
-                    hapResult = HapGetFrameTextureFormat(packet.data, packet.size, 0, &textureFormat);
+                    hapResult = HapGetFrameTextureFormat(packet->data, packet->size, 0, &textureFormat);
 #if OFX_HAP_HAS_CODECPAR
                     if (hapResult == HapResult_No_Error && !ofxHapPY::frameMatchesStream(textureFormat, _videoStream->codecpar->codec_tag))
 #else
@@ -428,8 +431,8 @@ void ofxHapPlayer::update(ofEventArgs & args)
                             _decodedFrame.buffer.resize(length);
                         }
                         unsigned long bytesUsed;
-                        hapResult = HapDecode(packet.data,
-                                              packet.size,
+                        hapResult = HapDecode(packet->data,
+                                              packet->size,
                                               0,
                                               ofxHapPY::doDecode,
                                               NULL,
@@ -441,15 +444,15 @@ void ofxHapPlayer::update(ofEventArgs & args)
                 }
                 if (hapResult == HapResult_No_Error)
                 {
-                    _decodedFrame.pts = packet.pts;
-                    _decodedFrame.duration = packet.duration;
+                    _decodedFrame.pts = packet->pts;
+                    _decodedFrame.duration = packet->duration;
                     _wantsUpload = true;
                 }
                 else
                 {
                     _decodedFrame.invalidate();
                 }
-                av_packet_unref(&packet);
+                av_packet_free(&packet);
             }
         }
     }

commit 178112d4f3e16757fddf98f8da990f249343a7dc

fix for older ffmpeg libraries
broken in 8dadcce9afd78582b815b22f7e47ae973c65b333

libs/ofxHap/src/AudioResampler.cpp
@@ -40,7 +40,7 @@ ofxHap::AudioResampler::AudioResampler(const AudioParameters& params, int outRat
 #if OFX_HAP_HAS_CHANNEL_LAYOUT
 _layout(&params.parameters->ch_layout),
 #else
-_layout(&params.parameters->channel_layout),
+_layout(params.parameters->channel_layout),
 #endif
 _sampleRateIn(params.parameters->sample_rate), _format(params.parameters->format),
 #else